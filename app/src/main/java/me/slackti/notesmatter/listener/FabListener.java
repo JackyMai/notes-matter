9107cbb0b9e86e2b9988880599d66386661bde9e
Jacky Mai <jacky.jy.mai@gmail.com>
2017-01-21 22:28:09 +1300

    Enter key will add item when EditText is not empty, cancels dialog otherwise

--- app/src/main/java/me/slackti/notesmatter/listener/FabListener.java
+++ app/src/main/java/me/slackti/notesmatter/listener/FabListener.java
@@ -3,16 +3,20 @@ package me.slackti.notesmatter.listener;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.renderscript.ScriptGroup;
+import android.inputmethodservice.KeyboardView;
 import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import me.slackti.notesmatter.R;
 import me.slackti.notesmatter.adapter.TodoAdapter;
@@ -22,6 +26,8 @@ import me.slackti.notesmatter.model.Todo;
 public class FabListener implements View.OnClickListener {
     private Context context;
     private TodoAdapter adapter;
+    private AlertDialog dialog;
+    private EditText editText;
 
     public FabListener(Context context, TodoAdapter adapter) {
         this.context = context;
@@ -37,16 +43,15 @@ public class FabListener implements View.OnClickListener {
 
         builder.setView(dialog_view);
 
+        dialog = builder.create();
+
         final Button add_button = (Button) dialog_view.findViewById(R.id.dialog_add_button);
         add_button.setEnabled(false);
 
         Button cancel_button = (Button) dialog_view.findViewById(R.id.dialog_cancel_button);
 
-        final EditText editText = (EditText) dialog_view.findViewById(R.id.dialog_todo_text);
+        editText = (EditText) dialog_view.findViewById(R.id.dialog_todo_text);
         editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if(s.toString().trim().isEmpty()) {
@@ -54,13 +59,16 @@ public class FabListener implements View.OnClickListener {
                 } else {
                     add_button.setEnabled(true);
                 }
+
+                checkForNewLine(s, start, count);
             }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
             public void afterTextChanged(Editable s) {}
         });
-
-        final AlertDialog dialog = builder.create();
 
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,8 +87,25 @@ public class FabListener implements View.OnClickListener {
             }
         });
 
+        // getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
+    }
+
+    private void checkForNewLine(CharSequence s, int start, int count) {
+        int end = start + count;
+
+        for(int i = start; i < end; i++) {
+            if (s.charAt(i) == '\n') {      // Enter key detected
+                String input = editText.getText().toString().trim();
+                if (input.isEmpty()) {      // If string is empty, close dialog
+                    dialog.cancel();
+                } else {
+                    adapter.addItem(new Todo(input));
+                    dialog.dismiss();
+                }
+            }
+        }
     }
 }
