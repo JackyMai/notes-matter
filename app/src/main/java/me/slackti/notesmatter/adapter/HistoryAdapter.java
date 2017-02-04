36360e9dd6bc70243a783154e0e8c651b633fd20
Jacky Mai <jacky.jy.mai@gmail.com>
2017-02-04 23:57:46 +1300

    Implemented getCompletedItems()

--- app/src/main/java/me/slackti/notesmatter/adapter/HistoryAdapter.java
+++ app/src/main/java/me/slackti/notesmatter/adapter/HistoryAdapter.java
@@ -2,7 +2,9 @@ package me.slackti.notesmatter.adapter;
 
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +17,6 @@ import me.slackti.notesmatter.model.TodoHolder;
 
 public class HistoryAdapter extends RecyclerView.Adapter<TodoHolder> {
 
-    private Context context;
-
     private LayoutInflater inflater;
     private ArrayList<Todo> todoList;
     private DatabaseHelper databaseHelper;
@@ -24,8 +24,6 @@ public class HistoryAdapter extends RecyclerView.Adapter<TodoHolder> {
     private int selectedPos = -1;
 
     public HistoryAdapter(Context context) {
-        this.context = context;
-
         inflater = LayoutInflater.from(context);
         todoList = new ArrayList<>();
         databaseHelper = new DatabaseHelper(context);
@@ -43,15 +41,27 @@ public class HistoryAdapter extends RecyclerView.Adapter<TodoHolder> {
 
     @Override
     public void onBindViewHolder(TodoHolder holder, int position) {
-
+        Todo todo = todoList.get(position);
+        holder.setTitle(todo.getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return todoList.size();
     }
 
     private void getCompletedItems() {
+        Cursor listData = databaseHelper.getCompletedItems();
 
+        if(listData.getCount() > 0) {
+            while(listData.moveToNext()) {
+                Todo todo = new Todo(listData.getString(0),
+                        listData.getString(1),
+                        listData.getInt(2),
+                        listData.getInt(3) != 0);   // Convert int to boolean
+
+                todoList.add(todo);
+            }
+        }
     }
 }
