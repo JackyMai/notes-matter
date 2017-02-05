b63807095dc02ee1ddf3cb2c010b2eaf5f022fc7
Jacky Mai <jacky.jy.mai@gmail.com>
2017-02-06 00:45:20 +1300

    Added adapter to HistoryActivity

--- app/src/main/java/me/slackti/notesmatter/ui/HistoryActivity.java
+++ app/src/main/java/me/slackti/notesmatter/ui/HistoryActivity.java
@@ -1,14 +1,18 @@
 package me.slackti.notesmatter.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import me.slackti.notesmatter.R;
 import me.slackti.notesmatter.adapter.HistoryAdapter;
+import me.slackti.notesmatter.listener.UndoneButtonListener;
+
+import static android.view.View.GONE;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -20,7 +24,13 @@ public class HistoryActivity extends AppCompatActivity {
         setTitle("History");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        HistoryAdapter adapter = new HistoryAdapter(this);
+        final RelativeLayout actionBar = (RelativeLayout) findViewById(R.id.history_action_bar);
+        actionBar.setVisibility(GONE);
+
+        HistoryAdapter adapter = new HistoryAdapter(this, actionBar);
+
+        ImageButton undoneButton = (ImageButton) findViewById(R.id.undone_button);
+        undoneButton.setOnClickListener(new UndoneButtonListener(adapter));
 
         RecyclerView recView = (RecyclerView) findViewById(R.id.history_list);
         recView.setLayoutManager(new LinearLayoutManager(this));
