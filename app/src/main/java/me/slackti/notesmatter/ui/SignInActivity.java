8d011f9d85f5b0e9815e143b21b7cab888b813b0
Jacky Mai <jacky.jy.mai@gmail.com>
2017-02-26 22:55:36 +1300

    Added empty layout for SignInActivity

--- app/src/main/java/me/slackti/notesmatter/ui/SignInActivity.java
+++ app/src/main/java/me/slackti/notesmatter/ui/SignInActivity.java
@@ -26,7 +26,7 @@ public class SignInActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_launcher);
+//        setContentView(R.layout.activity_sign_in);
 
         user = auth.getCurrentUser();
         if (user != null) {
