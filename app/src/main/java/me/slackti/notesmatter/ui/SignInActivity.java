af767a61440b84c417ba9cec5e35fadb30389d40
Jacky Mai <jacky.jy.mai@gmail.com>
2017-02-28 23:01:47 +1300

    SignInActivity is now invisble when starting up

--- app/src/main/java/me/slackti/notesmatter/ui/SignInActivity.java
+++ app/src/main/java/me/slackti/notesmatter/ui/SignInActivity.java
@@ -1,5 +1,6 @@
 package me.slackti.notesmatter.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +17,7 @@ import java.util.Arrays;
 
 import me.slackti.notesmatter.R;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends Activity {
 
     private final FirebaseAuth auth = FirebaseAuth.getInstance();
     private  FirebaseUser user;
@@ -26,7 +27,7 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+//        setContentView(R.layout.activity_sign_in);
 
         user = auth.getCurrentUser();
         if (user != null) {
